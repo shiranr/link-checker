49e2c2d27e7f459fcf14dc2d39793926a41858fd
shiranrubin <shiranrubin@microsoft.com>
2024-04-12 13:52:12 +0100

    upgrade colly

--- models/url_handler.go
+++ models/url_handler.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/v2"
 	log "github.com/sirupsen/logrus"
 	"strings"
 )
--- tests/integration_test.go
+++ tests/integration_test.go
@@ -20,9 +20,9 @@ func TestFilesWithError(t *testing.T) {
 	configPath := basepath + "/resources/linkcheck.json"
 	utils.LoadConfiguration(configPath)
 	readmeFiles := utils.ExtractMarkdownFiles()
+	logFileContent := readLogFile(logFilePath)
 	res := models.GetFilesProcessorInstance().Process(readmeFiles)
 	assert.ErrorContains(t, res, "ERROR: 3 links check failed, please check the logs")
-	logFileContent := readLogFile(logFilePath)
 	assert.Contains(t, logFileContent, "Went through 2 files")
 	assert.Contains(t, logFileContent, "Line 21 link nla.go status 400")
 	assert.Contains(t, logFileContent, "Line 33 link source-control/merge-strategies.md status 400")
